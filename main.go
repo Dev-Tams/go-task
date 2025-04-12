06eaf3191f558a9edce94dbd80311dbce26867b7
Dev-Tams <tammyalemu@gmail.com>
2025-04-12 10:51:54 +0100

    formatting strings in go

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
+
+    task := "learning go!"
+    name := "Tami"
     fmt.Println("Hello, World!")
+    
+    fmt.Println("My name is ", name, "and i took on a new task;", task)
+    fmt.Printf("My name is %v and i took on a new task; %v \n", name, task)
+
+    var str = fmt.Sprintf("My name is %v and i took on a new task; %v", name, task)
+
+    fmt.Print(str)
+
+
 }
