b42d16e3694b0fe5e46534b2a3fc04b42a9ba963
Dev-Tams <tammyalemu@gmail.com>
2025-04-25 01:11:33 +0100

    inheritance

--- main.go
+++ main.go
@@ -18,9 +18,32 @@ func main() {
 
     names := []string{"bob", "sue", "alice"}
     printStrings(names...)
+
+    harryPotterAggregator := concatter()
+	harryPotterAggregator("Mr.")
+	harryPotterAggregator("and")
+	harryPotterAggregator("Mrs.")
+	harryPotterAggregator("Dursley")
+	harryPotterAggregator("of")
+	harryPotterAggregator("number")
+	harryPotterAggregator("four,")
+	harryPotterAggregator("Privet")
+
+	fmt.Println(harryPotterAggregator("Drive"))
 }
 func printStrings(strings ...string) {
     for i := 0; i < len(strings); i++ {
         fmt.Println(strings[i])
     }
+
 }
+
+func concatter() func(string) string {
+	doc := ""
+	return func(word string) string {
+		doc += word + " "
+		return doc
+	}
+}
+
+
